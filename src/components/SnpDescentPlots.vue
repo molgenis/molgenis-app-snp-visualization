1371f2ef42fcf03064b883a83e9fc1be7ff59171
Mark-de-Haan <markdehaan90@gmail.com>
2017-10-11 17:20:03 +0200

    Save SVG elements as png

--- src/components/SnpDescentPlots.vue
+++ src/components/SnpDescentPlots.vue
@@ -65,8 +65,7 @@
   import { mapState } from 'vuex'
   import { SET_PARSED_DEF_OBJ, SET_DATA_INDEX } from '../store/mutations'
   import * as d3 from 'd3'
-  import JsPDF from 'jspdf-yworks'
-  import svg2pdf from 'svg2pdf.js'
+  import { saveSvgAsPng } from 'save-svg-as-png'
 
   export default {
     name: 'snp-descent-plot',
@@ -117,26 +116,10 @@
       },
       onDownloadButtonClick () {
         const svgElements = document.querySelectorAll('div>.plot-container>svg')
-        console.log(svgElements)
-        const width = 1000
-        const height = 1000
-
-        // create a new jsPDF instance
-        const pdf = new JsPDF('l', 'pt', [width, height])
-
-        // render the svg element
-        let yOffset = 0
         svgElements.forEach(svgElement => {
-          console.log(yOffset, 'render')
-          svg2pdf(svgElement, pdf, {
-            xOffset: 0,
-            yOffset: yOffset,
-            scale: 1
-          })
-          yOffset += 300
+          const name = svgElement.getElementsByTagName('g')[0].id + '.png'
+          saveSvgAsPng(svgElement, name)
         })
-
-        pdf.save('test-svg.pdf')
       },
       clear () {
         this.results = {}
