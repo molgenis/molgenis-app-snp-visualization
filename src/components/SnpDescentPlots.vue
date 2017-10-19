9b5a26627d49adecafc62c84816c6f14b88fce95
mkslofstra <mariska-karina@hotmail.com>
2017-10-19 13:48:02 +0200

    bump up max lines, just to be sure everything gets plotted

--- src/components/SnpDescentPlots.vue
+++ src/components/SnpDescentPlots.vue
@@ -140,7 +140,7 @@
         this.isDisplayPlots = true
         this.status = 'Processing data'
         this.t0 = performance.now()
-        const maxLines = 1000000
+        const maxLines = 10000000
         lineReader.readSomeLines(this.dataFile, maxLines, this.forEachLine, this.onComplete)
       },
       clear () {
