90f88fdb26a044698e5db4af101254e2241e8589
connoratrug <c.stroomberg@rug.nl>
2021-04-12 15:30:39 +0200

    fix: configure public path

---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    outputDir: 'dist',
+    publicPath: process.env.NODE_ENV === 'production' ? '@molgenis-ui/snp-visualization/dist/' : '/',
+    configureWebpack: {
+      devtool: 'source-map'
+    }
+  }
